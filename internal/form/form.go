5c2f352bd5fc7170c018d34af8e2894e2d503275
javier162380 <javierllorente16@gmail.com>
2021-11-11 19:12:39 +0100

    modify the generic form

--- internal/form/form.go
+++ internal/form/form.go
@@ -7,55 +7,54 @@ import (
 	"rent-a-thingy/internal/models"
 )
 
-func ExecuteForm() {
+func genericForm() (models.RequestMetadata, error) {
 
 	cityName, err := argSelectorWithOptions("Select City", models.Cities, "City not available", true)
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-
+		return models.RequestMetadata{}, err
 	}
 
 	cityMetadata, err := models.GetCityMetadata(cityName)
 
 	if err != nil {
-		fmt.Printf("%s", err)
+		return models.RequestMetadata{}, err
 	}
 
 	engineName, err := argSelectorWithOptions("Select engine", cityMetadata.Engines, "Site not found", true)
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		return models.RequestMetadata{}, err
 	}
 
 	engineMetadata, err := models.GetEngineMetadata(engineName, cityName)
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		return models.RequestMetadata{}, err
 	}
 
 	zipCodeOrDistricts, err := argSelectorWithOptions("Select ZipCode or Districts", engineMetadata.ZipCodesOrDistricts, "ZipCode or Districts not found", true)
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		return models.RequestMetadata{}, err
 	}
 
 	sortCategory, err := argSelectorWithOptions("Sort Information by", engineMetadata.SortBy, "Not allowed value", true)
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		return models.RequestMetadata{}, err
 	}
 
 	maxPrice, err := argSelector("Select max price", "Not allowed value")
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		return models.RequestMetadata{}, err
 	}
 
 	minPrice, err := argSelector("Select min price", "Not allowed value")
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		return models.RequestMetadata{}, err
 	}
 
 	genericMetadata := models.RequestMetadata{
@@ -67,14 +66,26 @@ func ExecuteForm() {
 		MinPrice:           minPrice,
 	}
 
-	formInstance := NewEngineForm(engineName)
-	enrichMetadata, err := formInstance.MutateRequestMetadata(genericMetadata)
+	return genericMetadata, nil
+
+}
+
+func ExecuteForm() {
+
+	requestMetadata, err := genericForm()
 
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 	}
 
-	engineInstance := engine.NewEngine(engineName)
+	formInstance := NewEngineForm(requestMetadata.Engine)
+	enrichMetadata, err := formInstance.MutateRequestMetadata(requestMetadata)
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+	}
+
+	engineInstance := engine.NewEngine(requestMetadata.Engine)
 
 	url := engineInstance.BuildUrl(enrichMetadata)
 
